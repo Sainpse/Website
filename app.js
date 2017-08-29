f5deb6e1b128b561bf0b9da39cdec20d1a2ac92b
MarcusMadumo <marcusmadumo.k@gmail.com>
2017-08-29 21:31:08 +0200

    Chatbot accounting new

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/', index);
+app.use('/', abchat);
 app.use('/users', users);
 app.use('/ab', abchat);
 
